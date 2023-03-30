ab46478afb7e6ab4a00341b4a51a56792df2a361
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2023-03-30 15:50:08 -0300

    feat: Adiciona extratores de dados para as API's do IMDB e Nasa

---
+++ ConsumindoAPiNasa/src/main/java/org/codigo/refatorando/dataExtract/NasaDataExtract.java
@@ -0,0 +1,30 @@
+package org.codigo.refatorando.dataExtract;
+
+
+import org.codigo.refatorando.JsonParser.JsonParser;
+import org.codigo.refatorando.classes.Conteudo;
+import org.codigo.refatorando.interfaces.DataExtract;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class NasaDataExtract implements DataExtract {
+
+    @Override
+    public List<Conteudo> dataExtract(String json){
+        var jsonParser = new JsonParser().parse(json);
+
+        List<Conteudo> conteudos = new ArrayList<>();
+
+        for(Map<String,String> atributos : jsonParser){
+            String title = atributos.get("title");
+            String urlImage = atributos.get("url");
+
+            var conteudo = new Conteudo(title, urlImage);
+            conteudos.add(conteudo);
+        }
+
+        return conteudos;
+    }
+}
